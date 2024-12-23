4289d5522261c53e66354a90487d98caa4ff97ac
Anthony Headley <agheadley@gmail.com>
2024-12-23 14:47:32 +0000

    a1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<div class="container">
+
+	
+	<header>
+
+	</header>
+	<main>
+		{@render children()}
+	</main>
+
+	<footer>
+	
+	</footer>
+	
+</div>
+</div>
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="" content="Supabase demo..." />
+</svelte:head>
+
+<p class="notice">
+	Testing supabase edge and drizzle ...
+</p>
+
+<style>
+
+</style>
